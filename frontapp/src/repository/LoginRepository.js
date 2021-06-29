130f6689e9e6f88c0b89ce0fd87832edbd272f17
밍로그 <fnfndia96@naver.com>
2021-06-29 13:37:20 +0000

    Rest api호출 방식 예제

--- frontapp/src/repository/LoginRepository.js
+++ frontapp/src/repository/LoginRepository.js
@@ -1,1 +1,23 @@
-// import axios from 'axios';
+// axios를 사용하려면 web pack을 설치해야할지도 모르겠음.. (devServerProperties 설정 필요)
+
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class LoginRepository extends Component {
+	
+	
+	// get방식
+	axios.get('/item').then((response) => response.data)
+	.then((items) => items.map((item) => {
+		console.log(item);
+	}));
+
+
+	// post방식
+	const json = {
+		id:"lee",
+		pw:"1111"
+	};
+	axios.post('/item', json).then((response) => console.log(response.data)); 
+	
+}
